991759317952d5a25fc9f835597a5fb6fa4275e3
fatoumatazahra2005-art <fatoumatazahra2005@gmail.com>
2026-02-04 21:54:59 +0100

    Definition du modele user

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,3 +1,66 @@
 class User {
-  
+  final String id ;
+
+  final String name ;
+  final String email ;
+  final String password ;
+  final String? avatar ;
+
+  final DateTime createdAt;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    this.avatar,
+    DateTime? createdAt,
+
+  }):createdAt = createdAt ?? DateTime.now();
+
+  User copyWith({
+    String? id,
+    String? name,
+    String? email,
+    String? password,
+    String? avatar,
+    DateTime? createdAt ,
+})
+  {
+    return User(
+        id : id ?? this.id,
+        name :name ?? this.name,
+        email :email ??  this.email,
+        password:this.password,
+        avatar:this.avatar,
+        createdAt: createdAt ?? this.createdAt);
+
+  }
+  Map<String , dynamic> toMap(){
+    return{
+      "id":id,
+      "name":name,
+      "email":email,
+      "password":password,
+      "avatar":avatar,
+      "createdAt":createdAt
+    };
+  }
+
+  factory User.fromMap(Map<String , dynamic> map){
+    return User(
+        id : map["id"] as String,
+        name :map["name"] as String,
+        email :map["email"] as String,
+        password:map["password"] as String,
+        avatar:map["avatar"] as String,
+        createdAt:DateTime.parse(map["createdAt"] as String)
+    );
+  }
+  @override
+  String toString() {
+    return 'User(id:$id , name:$name , email:$email )';
+  }
 }
+
+
